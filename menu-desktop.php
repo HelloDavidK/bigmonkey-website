20e7250abc36844b1b43a95406283a3960381022
HelloDavidK <davidk2519@gmail.com>
2026-03-30 08:21:16 +0300

    Update menu-desktop.php

--- menu-desktop.php
+++ menu-desktop.php
@@ -1,38 +1,38 @@
 <nav class="desktop-menu">
     <ul>
         <li class="has-mega-menu">
-            <a href="collection.php">E-LIQUIDES <span class="menu-arrow">▼</span></a>
+            <a href="collection.php?cat=eliquides">E-LIQUIDES <span class="menu-arrow">▼</span></a>
             <div class="mega-menu">
                 <div class="mega-container">
                     <div class="mega-col">
                         <h3>PAR MARQUES</h3>
                         <div class="icon-grid">
-                            <a href="collection.php" class="icon-item"><img src="img/fruizee-icon.jpg" alt="Fruizee Madagascar"></a>
-                            <a href="collection.php" class="icon-item"><img src="img/e.tasty-icon.jpg" alt="E.Tasty"></a>
-                            <a href="collection.php" class="icon-item"><img src="img/swoke-icon.jpg" alt="Swoke"></a>
-                            <a href="collection.php" class="icon-item"><img src="img/tjuice-icon.jpg" alt="T-Juice"></a>
-                            <a href="collection.php" class="icon-item"><img src="img/le coq qui vape-icon.jpg" alt="Le Coq Qui Vape"></a>
-                            <a href="collection.php" class="icon-item"><img src="img/le french liquid-icon.jpg" alt="Le French Liquid"></a>
+                            <a href="collection.php?cat=eliquides&amp;brand[]=Fruizee" class="icon-item"><img src="img/fruizee-icon.jpg" alt="Fruizee"></a>
+                            <a href="collection.php?cat=eliquides&amp;brand[]=E.Tasty" class="icon-item"><img src="img/e.tasty-icon.jpg" alt="E.Tasty"></a>
+                            <a href="collection.php?cat=eliquides&amp;brand[]=Swoke" class="icon-item"><img src="img/swoke-icon.jpg" alt="Swoke"></a>
+                            <a href="collection.php?cat=eliquides&amp;brand[]=T-Juice" class="icon-item"><img src="img/tjuice-icon.jpg" alt="T-Juice"></a>
+                            <a href="collection.php?cat=eliquides&amp;brand[]=Le%20Coq%20Qui%20Vape" class="icon-item"><img src="img/le coq qui vape-icon.jpg" alt="Le Coq Qui Vape"></a>
+                            <a href="collection.php?cat=eliquides&amp;brand[]=Le%20French%20Liquid" class="icon-item"><img src="img/le french liquid-icon.jpg" alt="Le French Liquid"></a>
                         </div>
-                        <a href="collection.php" class="all-link">Voir toutes les marques</a>
+                        <a href="collection.php?cat=eliquides" class="all-link">Voir toutes les marques</a>
                     </div>
                     <div class="mega-col">
                         <h3>PAR CONTENANCES</h3>
                         <div class="icon-grid text-grid">
-                            <a href="collection.php" class="icon-item"><img src="img/10ml-icon.svg" alt="E-liquide 10ml"><span>10ml</span></a>
-                            <a href="collection.php" class="icon-item"><img src="img/50ml-icon.svg" alt="E-liquide 50ml"><span>50ml</span></a>
-                            <a href="collection.php" class="icon-item"><img src="img/100ml-icon.svg" alt="E-liquide 100ml"><span>100ml</span></a>
+                            <a href="collection.php?cat=eliquides&amp;contenance[]=10" class="icon-item"><img src="img/10ml-icon.svg" alt="E-liquide 10ml"><span>10ml</span></a>
+                            <a href="collection.php?cat=eliquides&amp;contenance[]=50" class="icon-item"><img src="img/50ml-icon.svg" alt="E-liquide 50ml"><span>50ml</span></a>
+                            <a href="collection.php?cat=eliquides&amp;contenance[]=100" class="icon-item"><img src="img/100ml-icon.svg" alt="E-liquide 100ml"><span>100ml</span></a>
                         </div>
                     </div>
                     <div class="mega-col">
                         <h3>PAR SAVEURS</h3>
                         <div class="icon-grid text-grid">
-                            <a href="collection.php" class="icon-item"><img src="img/classic-icon.svg" alt="Saveur Classic"><span>Classic</span></a>
-                            <a href="collection.php" class="icon-item"><img src="img/gourmand-icon.svg" alt="Saveur Gourmande"><span>Gourmand</span></a>
-                            <a href="collection.php" class="icon-item"><img src="img/fruite-icon.svg" alt="Saveur Fruitée"><span>Fruité</span></a>
-                            <a href="collection.php" class="icon-item"><img src="img/fruité frais-icon.svg" alt="Saveur Fraîche"><span>Fruité Frais</span></a>
-                            <a href="collection.php" class="icon-item"><img src="img/mentholé-icon.svg" alt="Saveur Menthe"><span>Mentholé</span></a>
-                            <a href="collection.php" class="icon-item"><img src="img/boisson-icon.svg" alt="Saveur Boisson"><span>Boisson</span></a>
+                            <a href="collection.php?cat=eliquides&amp;saveur[]=Classic" class="icon-item"><img src="img/classic-icon.svg" alt="Saveur Classic"><span>Classic</span></a>
+                            <a href="collection.php?cat=eliquides&amp;saveur[]=Gourmand" class="icon-item"><img src="img/gourmand-icon.svg" alt="Saveur Gourmande"><span>Gourmand</span></a>
+                            <a href="collection.php?cat=eliquides&amp;saveur[]=Fruité" class="icon-item"><img src="img/fruite-icon.svg" alt="Saveur Fruitée"><span>Fruité</span></a>
+                            <a href="collection.php?cat=eliquides&amp;saveur[]=Fruité%20Frais" class="icon-item"><img src="img/fruité frais-icon.svg" alt="Saveur Fraîche"><span>Fruité Frais</span></a>
+                            <a href="collection.php?cat=eliquides&amp;saveur[]=Mentholé" class="icon-item"><img src="img/mentholé-icon.svg" alt="Saveur Menthe"><span>Mentholé</span></a>
+                            <a href="collection.php?cat=eliquides&amp;saveur[]=Boisson" class="icon-item"><img src="img/boisson-icon.svg" alt="Saveur Boisson"><span>Boisson</span></a>
                         </div>
                     </div>
                 </div>
@@ -40,25 +40,25 @@
         </li>
 
         <li class="has-mega-menu">
-            <a href="collection.php">DIY <span class="menu-arrow">▼</span></a>
+            <a href="collection.php?cat=diy">DIY <span class="menu-arrow">▼</span></a>
             <div class="mega-menu">
                 <div class="mega-container">
                     <div class="mega-col">
                         <h3>ACCESSOIRES DIY</h3>
                         <div class="icon-grid text-grid">
-                            <a href="collection.php" class="icon-item"><img src="img/concentré-icon.svg" alt="Concentrés DIY"><span>Concentrés</span></a>
-                            <a href="collection.php" class="icon-item"><img src="img/Materiel DIY-icon.svg" alt="Matériels DIY"><span>Matériels DIY</span></a>
-                            <a href="collection.php" class="icon-item"><img src="img/additif-icon.svg" alt="Additifs DIY"><span>Additifs</span></a>
-                            <a href="collection.php" class="icon-item"><img src="img/base-icon.svg" alt="Bases e-liquide"><span>Bases</span></a>
-                            <a href="collection.php" class="icon-item"><img src="img/booster-icon.svg" alt="Boosters Nicotine"><span>Boosters</span></a>
+                            <a href="collection.php?cat=diy&amp;q=concentr%C3%A9" class="icon-item"><img src="img/concentré-icon.svg" alt="Concentrés DIY"><span>Concentrés</span></a>
+                            <a href="collection.php?cat=diy&amp;q=mat%C3%A9riel" class="icon-item"><img src="img/Materiel DIY-icon.svg" alt="Matériels DIY"><span>Matériels DIY</span></a>
+                            <a href="collection.php?cat=diy&amp;q=additif" class="icon-item"><img src="img/additif-icon.svg" alt="Additifs DIY"><span>Additifs</span></a>
+                            <a href="collection.php?cat=diy&amp;q=base" class="icon-item"><img src="img/base-icon.svg" alt="Bases e-liquide"><span>Bases</span></a>
+                            <a href="collection.php?cat=diy&amp;q=booster" class="icon-item"><img src="img/booster-icon.svg" alt="Boosters Nicotine"><span>Boosters</span></a>
                         </div>
                     </div>
                     <div class="mega-col">
                         <h3>NOS E-LIQUIDES DIY</h3>
                         <div class="icon-grid text-grid">
-                            <a href="collection.php" class="icon-item"><img src="img/10ml DIY-icon.svg" alt="DIY 10ml"><span>10 ML</span></a>
-                            <a href="collection.php" class="icon-item"><img src="img/30ml DIY-icon.svg" alt="DIY 30ml"><span>30 ML</span></a>
-                            <a href="collection.php" class="icon-item"><img src="img/50ml DIY-icon.svg" alt="DIY 50ml"><span>50 ML</span></a>
+                            <a href="collection.php?cat=diy&amp;contenance[]=10" class="icon-item"><img src="img/10ml DIY-icon.svg" alt="DIY 10ml"><span>10 ML</span></a>
+                            <a href="collection.php?cat=diy&amp;contenance[]=30" class="icon-item"><img src="img/30ml DIY-icon.svg" alt="DIY 30ml"><span>30 ML</span></a>
+                            <a href="collection.php?cat=diy&amp;contenance[]=50" class="icon-item"><img src="img/50ml DIY-icon.svg" alt="DIY 50ml"><span>50 ML</span></a>
                         </div>
                     </div>
                 </div>
@@ -66,39 +66,39 @@
         </li>
 
         <li class="has-mega-menu">
-            <a href="collection.php">E-CIGARETTES <span class="menu-arrow">▼</span></a>
+            <a href="collection.php?cat=ecigarettes">E-CIGARETTES <span class="menu-arrow">▼</span></a>
             <div class="mega-menu">
                 <div class="mega-container">
                     <div class="mega-col">
                         <h3>KITS & BATTERIE</h3>
                         <div class="icon-grid text-grid">
-                            <a href="collection.php" class="icon-item"><img src="img/Kits complet-icon.svg" alt="Kits complets"><span>Kits complets</span></a>
-                            <a href="collection.php" class="icon-item"><img src="img/pod-icon.svg" alt="Pods"><span>Pods</span></a>
-                            <a href="collection.php" class="icon-item"><img src="img/box et batterie-icon.svg" alt="Box & Batteries"><span>Box & Batteries</span></a>
-                            <a href="collection.php" class="icon-item"><img src="img/puff-icon.svg" alt="Puffs"><span>Puffs</span></a>
+                            <a href="collection.php?cat=ecigarettes&amp;q=kit" class="icon-item"><img src="img/Kits complet-icon.svg" alt="Kits complets"><span>Kits complets</span></a>
+                            <a href="collection.php?cat=ecigarettes&amp;q=pod" class="icon-item"><img src="img/pod-icon.svg" alt="Pods"><span>Pods</span></a>
+                            <a href="collection.php?cat=ecigarettes&amp;q=box" class="icon-item"><img src="img/box et batterie-icon.svg" alt="Box & Batteries"><span>Box & Batteries</span></a>
+                            <a href="collection.php?cat=ecigarettes&amp;q=puff" class="icon-item"><img src="img/puff-icon.svg" alt="Puffs"><span>Puffs</span></a>
                         </div>
                     </div>
                     <div class="mega-col">
                         <h3>CLEAROS & ATOS</h3>
                         <div class="icon-grid text-grid">
-                            <a href="collection.php" class="icon-item"><img src="img/clearos-icon.svg" alt="Clearomiseurs"><span>Clearos</span></a>
-                            <a href="collection.php" class="icon-item"><img src="img/Atos-icon.svg" alt="Atomiseurs"><span>Atos</span></a>
+                            <a href="collection.php?cat=ecigarettes&amp;q=clearo" class="icon-item"><img src="img/clearos-icon.svg" alt="Clearomiseurs"><span>Clearos</span></a>
+                            <a href="collection.php?cat=ecigarettes&amp;q=ato" class="icon-item"><img src="img/Atos-icon.svg" alt="Atomiseurs"><span>Atos</span></a>
                         </div>
                     </div>
                     <div class="mega-col">
                         <h3>CARTOUCHES & RÉSISTANCES</h3>
                         <div class="icon-grid text-grid">
-                            <a href="collection.php" class="icon-item"><img src="img/cartouche-icon.svg" alt="Cartouches"><span>Cartouches</span></a>
-                            <a href="collection.php" class="icon-item"><img src="img/resistance-icon.svg" alt="Résistances"><span>Résistances</span></a>
+                            <a href="collection.php?cat=ecigarettes&amp;q=cartouche" class="icon-item"><img src="img/cartouche-icon.svg" alt="Cartouches"><span>Cartouches</span></a>
+                            <a href="collection.php?cat=ecigarettes&amp;q=r%C3%A9sistance" class="icon-item"><img src="img/resistance-icon.svg" alt="Résistances"><span>Résistances</span></a>
                         </div>
                     </div>
                 </div>
             </div>
         </li>
 
-        <li><a href="collection.php">NOUVEAUTÉS</a></li>
-        <li class="promo"><a href="collection.php">BONS PLANS</a></li>
-        
+        <li><a href="collection.php?cat=nouveautes">NOUVEAUTÉS</a></li>
+        <li class="promo"><a href="collection.php?cat=bons-plans">BONS PLANS</a></li>
+
         <li class="menu-search-container">
             <form action="collection.php" method="get">
                 <input type="text" name="q" placeholder="Recherche...">
